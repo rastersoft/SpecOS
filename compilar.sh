4e9f0a375aa7c98a4e878f15bf072c2bb89db861
Sergio Costas <raster@rastersoft.com>
2016-01-11 23:22:59 +0100

    Now sets the round-robin flag fine

--- compilar.sh
+++ compilar.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-z80asm -v -b kernel.asm && ./bin2z80 kernel.bin kernel.z80 23296 23553 && fbzx kernel.z80
+z80asm -v -b kernel.asm && ./bin2z80 kernel.bin kernel.z80 23296 23296 && fbzx kernel.z80
+cat kernel.err
